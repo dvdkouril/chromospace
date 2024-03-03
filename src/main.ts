a2c61ced399f24219ef761bc7ed4687e117cab5c
David Kouril <david.kouril@gmail.com>
2024-03-03 14:32:52 -0500

    wip: basic renderer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import './style.css'
+import { ChromatinBasicRenderer } from "./renderer/ChromatinBasicRenderer.ts";
 
 let appEl = document.querySelector<HTMLDivElement>('#app')!;
+const w = 800;
+const h = 600;
 
-appEl.innerHTML = `hello`;
+//~ create renderer
+const renderer = new ChromatinBasicRenderer();
+
+//~ add canvas to the page
+if (renderer.canvasElement) {
+    renderer.canvasElement.width = w;
+    renderer.canvasElement.height = h;
+    appEl.appendChild(renderer.canvasElement);
+}
+
+//~ initialize render loop
+renderer.startDrawing();
+
 
--- src/renderer/ChromatinBasicRenderer.ts
+++ src/renderer/ChromatinBasicRenderer.ts
@@ -1,1 +1,25 @@
-test
+export class ChromatinBasicRenderer {
+    canvasElement: HTMLCanvasElement | null;
+
+    constructor(canvas: HTMLCanvasElement | null = null) {
+        this.canvasElement = canvas;
+        
+        if (canvas == null) {
+            this.canvasElement = document.createElement('canvas');
+        }
+
+        this.render = this.render.bind(this);
+    }
+
+    startDrawing() {
+        requestAnimationFrame(this.render);
+    }
+
+    render() {
+        requestAnimationFrame(this.render);
+
+        console.log("drawing");
+
+        // threeRenderer.render();
+    }
+};
