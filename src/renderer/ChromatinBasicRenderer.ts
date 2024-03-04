eb6cfd850e1ab925a9e18bc59d1ceddb2ce626ee
David Kouril <david.kouril@gmail.com>
2024-03-04 15:10:24 -0500

    tubes and stuff

--- src/renderer/ChromatinBasicRenderer.ts
+++ src/renderer/ChromatinBasicRenderer.ts
@@ -1,6 +1,7 @@
-import { WebGLRenderer, Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
+import { WebGLRenderer, Scene, PerspectiveCamera, MeshBasicMaterial, Mesh, SphereGeometry, CylinderGeometry, Vector3, Euler, Quaternion, DirectionalLight, AmbientLight, MeshStandardMaterial } from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { ChromatinScene } from '../chromatin';
+import { ChromatinChunk, ChromatinScene } from '../chromatin';
+import { vec3 } from 'gl-matrix';
 
 export class ChromatinBasicRenderer {
     chromatinScene: ChromatinScene | undefined;
@@ -22,6 +23,19 @@ export class ChromatinBasicRenderer {
         this.camera.position.z = 5;
         controls.update();
 
+        const lightA = new DirectionalLight();
+        lightA.position.set(3, 10, 10);
+        lightA.castShadow = true;
+        // <T.DirectionalLight castShadow position={[3, 10, 10]} />
+        const lightB = new DirectionalLight();
+        lightB.position.set(-3, 10, -10);
+        lightB.intensity = 0.2;
+        // <T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
+        const lightC = new AmbientLight();
+        lightC.intensity = 0.2;
+        // <T.AmbientLight intensity={0.2} />
+        this.scene.add(lightA, lightB, lightC);
+
         this.render = this.render.bind(this);
         this.getCanvasElement = this.getCanvasElement.bind(this);
     }
@@ -33,21 +47,92 @@ export class ChromatinBasicRenderer {
     addScene(scene: ChromatinScene) {
         this.chromatinScene = scene;
 
-        const geometry = new BoxGeometry( 0.1, 0.1, 0.1 );
-        const material = new MeshBasicMaterial( { color: 0x00ff00 } );
-        const cube = new Mesh( geometry, material );
-
         for (let chunk of scene.chunks) {
-            for (let b of chunk.bins) {
-                // cube.position.set(b.x, b.y, b.z);
-                const binCube = cube.clone();
-                binCube.position.set(b[0], b[1], b[2]);
-                this.scene.add(binCube);
-            }
+            const meshes = this.buildPart(chunk);
+            this.scene.add(...meshes);
         }
 
         //~ TODO: scene.models
     }
+
+    buildPart(chunk: ChromatinChunk): Mesh[] {
+        const sphereGeometry = new SphereGeometry(0.08);
+        const tubeGeometry = new CylinderGeometry(0.05, 0.05, 1.0, 3, 1);
+
+        // const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+        // const color = 0x00ff00;
+        // const color = 0xeeeeee;
+        const color = 0xffffff;
+        const material = new MeshStandardMaterial( { color: color } );
+
+        const binSphere = new Mesh(sphereGeometry, material);
+        const binTube = new Mesh(tubeGeometry, material);
+
+        let meshes: Mesh[] = [];
+        //~ bin spheres
+        for (let b of chunk.bins) {
+            const s = binSphere.clone();
+            s.position.set(b[0], b[1], b[2]);
+            meshes.push(s);
+
+        }
+        //~ tubes between tubes
+        for (let tube of this.computeTubes(chunk.bins)) {
+            const t = binTube.clone();
+            t.position.set(tube.position.x, tube.position.y, tube.position.z);
+            t.rotation.set(tube.rotation.x, tube.rotation.y, tube.rotation.z, tube.rotation.order);
+            t.scale.setY(tube.scale);
+            meshes.push(t);
+        }
+
+        return meshes;
+    }
+
+    getRotationFromTwoPositions = (from: Vector3, to: Vector3) => {
+        const fromCopy = new Vector3(from.x, from.y, from.z);
+        const toCopy = new Vector3(to.x, to.y, to.z);
+        const q = new Quaternion();
+        const u = new Vector3(0, 1, 0);
+        const v = toCopy.sub(fromCopy).normalize();
+
+        q.setFromUnitVectors(u, v);
+
+        const eulers = new Euler();
+        return eulers.setFromQuaternion(q);
+    }
+
+    computeTubes = (bins: vec3[]) => {
+        const t: {position: Vector3, rotation: Euler, scale: number}[] = [];
+        for (let i = 0; i < bins.length - 1; i++) {
+            const first = new Vector3(bins[i][0], bins[i][1], bins[i][2]);
+            const second = new Vector3(
+                bins[i + 1][0],
+                bins[i + 1][1],
+                bins[i + 1][2]
+            );
+
+            //~ position between the two bins
+            const pos = new Vector3();
+            pos.subVectors(second, first);
+            pos.divideScalar(2);
+            pos.addVectors(first, pos);
+            const tubePosition = pos;
+            //~ rotation
+            const tubeRotation = this.getRotationFromTwoPositions(first, second);
+            //~ tube length
+            const betweenVec = new Vector3();
+            betweenVec.subVectors(second, first);
+            const tubeScale = betweenVec.length();
+
+            t.push({
+                position: tubePosition,
+                rotation: tubeRotation,
+                scale: tubeScale,
+            });
+        }
+
+        return t;
+    };
 
     startDrawing() {
         requestAnimationFrame(this.render);
