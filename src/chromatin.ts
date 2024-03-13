037170a0d3abb0aaac6714d438faf948518df575
David Kouril <david.kouril@gmail.com>
2024-03-13 13:15:22 -0400

    wip: fetching part api

--- src/chromatin.ts
+++ src/chromatin.ts
@@ -119,6 +119,91 @@ export function getRange(model: ChromatinModel, coordinates: string): ChromatinP
     // return null;
     return newPart
 }
+/*
+ * This is probably useful for what queries users might be interested in making: https://genome.ucsc.edu/goldenPath/help/query.html
+ */
+
+/*
+ * Fetched a bin range from model. 
+ * - absolute for the whole model: bins of each part are concatenated based on order of parts
+ */
+export function getBinsFromModel(model: ChromatinModel, start: number, end: number): ChromatinModel | null {
+    /*
+     * I actually have a choice here:
+     * 1) just make the selection into a big "anonymous" part, without any separation of different parts
+     * 2) maintain the separation into parts and essentially just return a new model
+     *
+     * now that I think about it, only 2) really makes sense: I can't concatenate two parts because that would create a connection.
+     */
+  
+    let newModel: ChromatinModel = {
+        ...model,
+        parts: [],
+    };
+    // let newPart: ChromatinPart | null = null;
+    let currentOffset = 0;
+    for (let p of model.parts) {
+        const startIndex = start - currentOffset;
+        const endIndex = Math.min(p.chunk.bins.length, end - currentOffset);
+        currentOffset = endIndex;
+        const newPart = {
+            chunk: {
+                ...p.chunk, //TODO: probably I'll want a different id...
+                bins: p.chunk.bins.slice(startIndex, endIndex),
+                rawBins: p.chunk.rawBins.slice(startIndex, endIndex),
+            },
+            coordinates: p.coordinates, //TODO: needs actually converting
+            resolution: p.resolution,
+        };
+        newModel.parts.push(newPart);
+    }
+
+    return newModel;
+}
+
+export function getBinsFromPart(part: ChromatinPart, start: number, end: number): ChromatinPart | null {
+
+    const clamp = (val: number, min: number, max: number) => Math.max(Math.min(max, val), min);
+
+    //~ range guards
+    const n = part.chunk.bins.length;
+    const startIndex = clamp(start, 0, n - 1);
+    const endIndex = clamp(end, 0, n - 1);
+
+    const newPart = {
+        chunk: {
+            ...part.chunk, //TODO: probably I'll want a different id...
+            bins: part.chunk.bins.slice(startIndex, endIndex),
+            rawBins: part.chunk.rawBins.slice(startIndex, endIndex),
+        },
+        coordinates: part.coordinates, //TODO: needs actually converting
+        resolution: part.resolution,
+    };
+    return newPart;
+}
+
+/*
+ * TODO: move elsewhere
+ * Utility function for converting genomic coordinate (i.e., nucleobase position) to bin index, given certain resolution
+ *
+ * --------
+ * Example:
+ * resolution: 10bp
+ * positions: 0123456789...         *
+ * sequence:  TCTGCGGAGCACTCTGGTAATGCATATGGTCCACAGGACATTCGTCGCTT
+ * bins:      ____0_____----1-----||||2|||||xxxx3xxxxx****4*****
+ * coordinateToBin(22, 10) -> 2
+ */
+function coordinateToBin(coordinate: number, resolution: number, sequenceOffset?: number): number {
+    if (!sequenceOffset) {
+        sequenceOffset = 0;
+    }
+    return Math.floor((coordinate - sequenceOffset) / resolution);
+}
+
+// function binToCoordinateStart(): number {
+//     return 0;
+// }
 
 export function display(scene: ChromatinScene): HTMLCanvasElement {
     const renderer = new ChromatinBasicRenderer();
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 export type { GenomicCoordinates, ChromatinChunk, ChromatinModel, ChromatinScene } from './chromatin.ts';
-export { addChunkToScene, addModelToScene, display, getRange } from './chromatin.ts';
+export { addChunkToScene, addModelToScene, display, getRange, getBinsFromModel, getBinsFromPart } from './chromatin.ts';
 
 export { parseTsv, parse3dg } from './data-loaders/tsv-parser.ts';
 
