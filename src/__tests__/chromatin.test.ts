18c1c24a4bccdf75daf80766a77b486634ea913a
David Kouril <david.kouril@gmail.com>
2024-03-29 19:40:10 -0400

    test: get chromosome

--- src/__tests__/chromatin.test.ts
+++ src/__tests__/chromatin.test.ts
@@ -1,5 +1,8 @@
 import { expect, test } from "vitest";
+
 import { coordinateToBin } from "../utils.ts";
+import { get } from "../chromatin.ts";
+import { parse3dg } from "../data-loaders/tsv-parser.ts";
 
 test("coordinateToBin simple", () => {
   //~ In an aligned sequence, and for bins with resolution
@@ -9,4 +12,30 @@ test("coordinateToBin simple", () => {
 
 test("coordinateToBin with start offset", () => {
   expect(coordinateToBin(33, 10, 20)).toBe(1);
+});
+
+
+const fetchTsv = async (url: string) => {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`fetch failed: ${response.status}`);
+    const fileContent = await response.text();
+    return fileContent;
+};
+
+test("get chromosome", async () => {
+  const urlTan2018 = "https://dl.dropboxusercontent.com/scl/fi/lzv3ba5paum6srhte4z2t/GSM3271406_pbmc_18.impute.3dg.txt?rlkey=dc7k1gg5ghv2v7dsl0gg1uoo9&dl=0";
+  const fileTan2018 = await fetchTsv(urlTan2018);
+  const testModel = parse3dg(fileTan2018 , { center: true, normalize: true });
+
+  expect(testModel).toBeDefined();
+
+  const chr15patPart = get(testModel!, "15(pat)");
+  expect(chr15patPart).toBeDefined();
+  expect(chr15patPart?.label).toBe("15(pat)");
+  expect(chr15patPart?.chunk.bins.length).toBe(825);
+
+  const chr1patPart = get(testModel!, "1(pat)");
+  expect(chr1patPart).toBeDefined();
+  expect(chr1patPart?.label).toBe("1(pat)");
+  expect(chr1patPart?.chunk.bins.length).toBe(2487);
 });
