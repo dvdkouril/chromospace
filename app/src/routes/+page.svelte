dc6a909742f111d9eebace8b452b30fdaf6d1bd6
David Kouril <david.kouril@gmail.com>
2024-06-28 12:44:57 +0200

    wip: not optimal, but somehow it works

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import ChromospaceApp from "$lib/ChromospaceApp.svelte";
+</script>
+
+<h1>chromospace</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<ChromospaceApp />
