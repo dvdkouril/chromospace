81538d8125786e68409416cf096bfc981442eca1
David Kouril <david.kouril@gmail.com>
2024-07-29 12:21:41 -0400

    add whole-genome model example

--- app/src/routes/examples/+page.svelte
+++ app/src/routes/examples/+page.svelte
@@ -1,5 +1,12 @@
 <div class="text-xl py-2">
 	<ul>
-		<li>Stevens et al.</li>
+		<li>
+			<a
+				class="text-blue-600 dark:text-blue-500 hover:underline"
+				href="https://observablehq.com/d/f73ed7234e20e30b"
+			>
+				Whole-genome human cell model from Tan et al. 2018</a
+			>
+		</li>
 	</ul>
 </div>
