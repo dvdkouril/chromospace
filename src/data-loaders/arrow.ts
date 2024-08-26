5423645a2e98c7354b486d21730fd3bb86587dd0
David Kouril <david.kouril@gmail.com>
2024-08-26 11:16:53 -0400

    refactor: unified arrow loader
    WIP: only the ChromatinChunk path is implemented in this commit

--- src/data-loaders/arrow.ts
+++ src/data-loaders/arrow.ts
@@ -1,4 +1,4 @@
-import { Float, Table, tableFromIPC } from 'apache-arrow';
+import { Table, Schema, Float, tableFromIPC } from 'apache-arrow';
 import { type LoadOptions, normalize, recenter } from "./loader-utils";
 import { ChromatinChunk, ChromatinModel } from '../chromatin-types';
 import { vec3 } from 'gl-matrix';
@@ -14,75 +14,95 @@ export async function loadFromURL(url: string, options: LoadOptions): Promise<Ch
       throw new Error(`Response status: ${response.status}`);
     }
     const buffer = await response.arrayBuffer();
-    console.log("heyyyyy");
     return load(buffer, options);
   } catch (err: any) {
     console.error(err.message);
-    return "whateverr";
+    return undefined;
   }
 }
 
 /**
-  * Loads a 3D chromatin model from memory. Uses Apache Arrow.
-  * TODO: Three options for input:
-  * - Parquet file: URL or local path
-  * - Arrow IPC
-  * - DataView (from Python)
+ * Will return true if the table schema contains only 3 columns named x, y, z
+ */
+function isChunk(tableSchema: Schema): boolean {
+  return tableSchema.fields.length === 3 && hasXYZ(tableSchema);
+}
+
+/** 
+ * Returns true when table schema contains x, y, z fields
+ */
+function hasXYZ(tableSchema: Schema): boolean {
+  const columnNames = tableSchema.fields.map(f => f.name);
+  return columnNames.includes("x") &&
+    columnNames.includes("y") &&
+    columnNames.includes("z");
+}
+
+/**
+ * Does a computation with the bin coordinates based on the options
+ * returns a copy of the passed array with the changes applied
+ */
+function processBins(bins: vec3[], options: LoadOptions): vec3[] {
+  let binsCopy = [...bins];
+  if (options.center) {
+    binsCopy = recenter(binsCopy);
+  }
+
+  if (options.normalize) {
+    binsCopy = normalize(binsCopy);
+  }
+  return binsCopy;
+}
+
+/**
+ * Turns the Arrow Table into a ChromatinChunk object
+ */
+function processTableAsChunk(table: Table<any>, options?: LoadOptions): ChromatinChunk {
+  const typedTable = table as Table<{ x: Float, y: Float, z: Float }>;
+  let bins: vec3[] = [];
+  for (let i = 0; i < typedTable.numRows; i++) {
+    const row = typedTable.get(i);
+    if (row) {
+      bins.push(vec3.fromValues(row.x, row.y, row.z));
+    } else {
+      console.error("Row of XYZ coords");
+    }
+  }
+
+  const rawBins = bins; //~ saving the original, unprocessed data
+  options = options || { center: true, normalize: true };
+  bins = processBins(bins, options);
+
+  return {
+    bins: bins,
+    rawBins: rawBins,
+    id: 0,
+  };
+}
+
+/**
+ * TODO:Turns the Arrow Table into a ChromatinModel object
+ */
+function processTableAsModel(): ChromatinModel {
+  return {
+    parts: [],
+  };
+}
+
+/**
+  * Loads a 3D chromatin model from memory using Apache Arrow
   */
 export function load(
   buffer: ArrayBuffer,
   options?: LoadOptions,
 ): ChromatinChunk | ChromatinModel {
   const bytes = new Uint8Array(buffer);
-  const table = tableFromIPC<{ x: Float, y: Float, z: Float }>(bytes);
-  console.log("loaded Parquet table, with #cols: " + table.numCols + " and #row: " + table.numRows);
+  const table = tableFromIPC(bytes);
+  console.log("loaded Array table, with #cols: " + table.numCols + " and #row: " + table.numRows);
 
-  const columnNames = table.schema.fields.map(f => f.name);
-  const hasXYZ =
-    columnNames.includes("x") &&
-    columnNames.includes("y") &&
-    columnNames.includes("z");
-  if (!hasXYZ) {
-    console.error("Arrow Table doesn't contain x, y, z coordinates!");
-  }
-
-  let bins: vec3[] = []; //~ TODO:
-  for (let i = 0; i < table.numRows; i++) {
-    const row = table.get(i)!;
-    row.x
-    const xCol = table.getChild("x");
-    const yCol = table.getChild("y");
-    const zCol = table.getChild("z");
-    if ((xCol != null) && (yCol != null) && (zCol != null)) {
-      const x = xCol.get(i) as number;
-      const y = yCol.get(i) as number;
-      const z = zCol.get(i) as number;
-
-      bins.push(vec3.fromValues(x, y, z));
-    }
-  }
-
-  const rawBins = bins;
-  options = options || { center: true, normalize: true };
-  if (options.center) {
-    bins = recenter(bins);
-  }
-
-  if (options.normalize) {
-    bins = normalize(bins);
-  }
-
-  //~ data will probably represent a model if a column named i.e., 'chromosome' is present. 
-  const isModel = (table.numCols > 3); // TODO: needs a better check (check for x, y, z etc.)
-  if (!isModel) { // -> ChromatinChunk
-    return {
-      bins: bins,
-      rawBins: rawBins,
-      id: 0,
-    };
+  if (isChunk(table.schema)) { // -> ChromatinChunk
+    return processTableAsChunk(table, options);
   } else { // -> ChromatinModel
-    return {
-      parts: [],
-    };
+    return processTableAsModel();
   }
 }
