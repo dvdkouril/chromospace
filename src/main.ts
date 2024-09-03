c3df8ddc18a8c54c0f450c4bdc91d268e4f36d48
David Kouril <david.kouril@gmail.com>
2024-09-03 16:23:20 -0400

    remove more loaders

--- src/main.ts
+++ src/main.ts
@@ -15,14 +15,6 @@ export {
   getBinsFromPart,
 } from "./selections.ts";
 
-export {
-  parseTsv,
-  parse3dg,
-  parseWeirdTsvFromMiniMDS,
-} from "./data-loaders/tsv-parser.ts";
-export { parsePDB, parseNumpyArray } from "./data-loaders/pdb-parser.ts";
-export { parseXYZ } from "./data-loaders/xyz-parser.ts";
-
 export { load, loadFromURL } from "./data-loaders/arrow.ts";
 
 export { ChromatinBasicRenderer } from "./renderer/ChromatinBasicRenderer.ts";
