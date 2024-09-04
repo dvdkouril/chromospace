8b29c4cf1293ba117ae72ace58bd4abc0d6de530
David Kouril <david.kouril@gmail.com>
2024-09-04 18:49:52 -0400

    forgotten import that doesn't exist anymore

--- app/src/lib/ChromospaceApp.svelte
+++ app/src/lib/ChromospaceApp.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
         import { onMount } from "svelte";
         import {
-                parseTsv,
                 addChunkToScene,
                 initScene,
                 display,
