2b5d821d2c35dae48e85b0c4891c7de3695b2179
David Kouril <david.kouril@gmail.com>
2025-06-20 14:22:04 -0400

    tsc: ignore errors for n8ao, doesn't have types

--- src/renderer/ChromatinBasicRenderer.ts
+++ src/renderer/ChromatinBasicRenderer.ts
@@ -1,8 +1,7 @@
-// @ts-ignore
-
 import type { Color as ChromaColor } from "chroma-js";
 import chroma from "chroma-js";
 import type { vec3 } from "gl-matrix";
+// @ts-ignore
 import { N8AOPostPass } from "n8ao";
 import {
   EffectComposer,
